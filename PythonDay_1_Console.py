1341c4a7e695884a56507293a196c86f68e105d0
ravi4all <ravikanttyagi.2295@gmail.com>
2018-01-13 15:41:22 +0530

    Add files via upload

---
+++ PythonDay_1_Console.py
@@ -0,0 +1,91 @@
+Python 3.6.2 (v3.6.2:5fd33b5, Jul  8 2017, 04:57:36) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print("Hello")
+Hello
+>>> a = 10
+>>> b = 12
+>>> c = a + b
+>>> print(c)
+22
+>>> a/b
+0.8333333333333334
+>>> a*b
+120
+>>> a-b
+-2
+>>> a//b
+0
+>>> a*a
+100
+>>> a*a*a
+1000
+>>> a**2
+100
+>>> a**3
+1000
+>>> a**10
+10000000000
+>>> type(a)
+<class 'int'>
+>>> a = "Hello"
+>>> a = 10
+>>> id(a)
+1694746576
+>>> id(10)
+1694746576
+>>> a = "Hello"
+>>> b = a
+>>> c = a
+>>> del a
+>>> a
+Traceback (most recent call last):
+  File "<pyshell#23>", line 1, in <module>
+    a
+NameError: name 'a' is not defined
+>>> b
+'Hello'
+>>> c
+'Hello'
+>>> del b
+>>> c = "hi"
+>>> a,b = 10,15
+>>> a,b = b,a
+>>> a
+15
+>>> b
+10
+>>> a = "Hello"
+>>> a*10
+'HelloHelloHelloHelloHelloHelloHelloHelloHelloHello'
+>>> 
+ RESTART: C:/Users/asus/Documents/Data/DataTransfer/BMPL_Batches/Jan_2018/Python_WE_2/PythonFirstProgram.py 
+22
+>>> a
+10
+>>> b
+12
+>>> c
+22
+>>> 
+ RESTART: C:/Users/asus/Documents/Data/DataTransfer/BMPL_Batches/Jan_2018/Python_WE_2/PythonFirstProgram.py 
+22
+Addition is 22
+>>> 
+ RESTART: C:/Users/asus/Documents/Data/DataTransfer/BMPL_Batches/Jan_2018/Python_WE_2/PythonFirstProgram.py 
+22
+Addition is 22
+Addition of 10 and 12 is 22
+>>> 
+ RESTART: C:/Users/asus/Documents/Data/DataTransfer/BMPL_Batches/Jan_2018/Python_WE_2/PythonFirstProgram.py 
+22
+Addition is 22
+Addition of 10 and 12 is 22
+Addition of 10 and 12 is 22
+>>> 
+ RESTART: C:/Users/asus/Documents/Data/DataTransfer/BMPL_Batches/Jan_2018/Python_WE_2/PythonFirstProgram.py 
+22
+Addition is 22
+Addition of 10 and 12 is 22
+Addition of 10 and 12 is 22
+Addition of 10 and 12 is 22.000000
+>>> 
---
+++ PythonFirstProgram.py
@@ -0,0 +1,16 @@
+a = 10
+b = 12
+c = a+b
+print(c)
+
+print("Addition is",c)
+
+print("Addition of",a,"and",b,"is",c)
+
+print("Addition of {} and {} is {}".format(a,b,c))
+
+print("Addition of %d and %s is %f"%(a,b,c))
+
+#type(a)
+
+#id(a)
